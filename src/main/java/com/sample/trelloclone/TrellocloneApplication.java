efa2435d39f0467ee5dded0ce9420570181baa62
Naveen <snaveen@outlook.com>
2023-03-10 14:53:04 +0530

    first commit

---
+++ src/main/java/com/sample/trelloclone/TrellocloneApplication.java
@@ -0,0 +1,13 @@
+package com.sample.trelloclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrellocloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrellocloneApplication.class, args);
+	}
+
+}
