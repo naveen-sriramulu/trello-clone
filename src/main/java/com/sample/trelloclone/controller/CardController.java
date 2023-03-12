d4e01a9faaf76ad540888860b9747ba3d6cb8176
Naveen <snaveen@outlook.com>
2023-03-13 00:18:53 +0530

    TRE-0002 implement REST API getCardsbyTag in CardController

--- src/main/java/com/sample/trelloclone/controller/CardController.java
+++ src/main/java/com/sample/trelloclone/controller/CardController.java
@@ -1,14 +1,21 @@
 package com.sample.trelloclone.controller;
 
 import com.sample.trelloclone.dto.CardDto;
+import com.sample.trelloclone.exception.EmptyCardsException;
 import com.sample.trelloclone.service.CardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -22,7 +29,19 @@ public class CardController {
     }
 
     @GetMapping("/tag/{tag}")
+    @Operation(summary = "Get cards by tag")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Return the matching cards",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = CardDto.class))}),
+            @ApiResponse(responseCode = "204", description = "No content",
+                    content = @Content)
+    })
     public ResponseEntity<List<CardDto>> getCardsByTag(@PathVariable(required = true) String tag) {
-        return ResponseEntity.ok(Collections.emptyList());
+        List<CardDto> cardsByTag = cardService.getCardsByTag(tag);
+        if (CollectionUtils.isEmpty(cardsByTag)) {
+            throw new EmptyCardsException();
+        }
+        return ResponseEntity.ok(cardsByTag);
     }
 }
